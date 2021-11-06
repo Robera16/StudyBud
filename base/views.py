2970db73ff8080a03a3df0896e3c24ee8d465eb1
robera16 <roberaworknehrobwork@gmail.com>
2021-11-06 13:38:43 +0300

    added templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 def room(request):
-    return HttpResponse('room page')
+    return render(request, 'room.html')
